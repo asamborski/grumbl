1d2197d8270c832f9091a3fb5070e7a2d7a87cf8
Anurag <anuragp122195@gmail.com>
2017-03-22 17:21:39 -0400

    remove config_secret.py

--- api_prototype/app/config_secret.py
+++
@@ -1,5 +0,0 @@
-yelp_auth = {
-    'client_id' : 'v5UQcYP7l0Sga33-J8wdOA',
-    'client_secret' : 'QvP6gyRvHE39riLBAjQ9bguJ79SlYefRnMV6QhfUkL3f09zBLXmhSQQRIADexNIY',
-    'grant_type' : 'client_credentials'
-}
