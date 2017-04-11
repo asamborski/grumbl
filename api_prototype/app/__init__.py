ef53c8c42097a45a300310c83cd6d28da26c9389
tanatsigwahungwe <thungwe@yahoo.com>
2017-04-11 01:00:12 -0400

    Update __init__.py

--- api_prototype/app/__init__.py
+++ api_prototype/app/__init__.py
@@ -1,10 +1,7 @@
 from flask import Flask
-from flask.ext.pymongo import PyMongo
 
 # Initialize the app
 app = Flask(__name__, instance_relative_config=True)
-# Initialize mongodb
-mongo = PyMongo(app)
 
 # Load the views
 from app import views
